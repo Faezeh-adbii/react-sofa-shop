ee2cb2156eb86c21b804d70a6c50c705791a465a
Faezeh <faezehadabi9@gmail.com>
2026-08-16 17:50:34 +0330

    feat: wrap App with Layout component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles/fonts.css";
+import "./styles/index.css";
+import App from "./App.jsx";
+import Layout from "./components/layout/Layout.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Layout>
+      <App />
+    </Layout>
   </StrictMode>,
-)
+);
