bbcad798ce18be4a1614d09c8ea677508b12f54c
Faezeh <faezehadabi9@gmail.com>
2026-08-17 11:05:23 +0330

    feat: add SearchHeader to Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Logo from "../Logo";
+import SearchHeader from "../SearchHeader";
 
 const headerClasses =
   "flex justify-center p-3 items-center bg-gradient-to-r from-pink-700/90 to-pink-950/90 backdrop-blur-sm rounded-b-[45px]";
@@ -7,12 +8,17 @@ const headerClasses =
 function Header() {
   return (
     <>
-      <header>
+      <header className="bg-gray-300/30">
         <div className={headerClasses}>
           <h4 className="text-xl font-bold text-white "> فروشگاه مبل آرا </h4>
         </div>
-        <div className="container">
-          <Logo/>
+        <div className="container flex justify-between pt-8">
+          <div className="w-full">
+            <Logo />
+          </div>
+          <div className=" w-full flex justify-center items-center">
+            <SearchHeader />
+          </div>
         </div>
       </header>
     </>
