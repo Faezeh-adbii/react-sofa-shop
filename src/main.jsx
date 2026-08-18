9b715d69ab557033ed397f300db82ada1d84ec56
Faezeh <faezehadabi9@gmail.com>
2026-08-18 18:53:22 +0330

    chore: configure Apollo Client for GraphQL data layer
    - Add Apollo Client dependencies
    - Setup GraphQL client with Hygraph endpoint
    - Configure environment variables for API URL
    - Provide client to entire app via ApolloProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,30 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/fonts.css";
-import "./styles/index.css";
-import App from "./App.jsx";
-import Layout from "./components/layout/Layout.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+import Layout from "./components/layout/Layout.jsx";
+import App from "./App.jsx";
+import "./styles/index.css";
+import "./styles/fonts.css";
+
+const httpLink = new HttpLink({
+  uri: import.meta.env.VITE_APP_GRAPHCMS,
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Layout>
-        <App />
-      </Layout>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Layout>
+          <App />
+        </Layout>
+      </BrowserRouter>
+    </ApolloProvider>
   </StrictMode>,
 );
