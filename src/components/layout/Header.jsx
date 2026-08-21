a0c100ce5e7f21855d16fa8881b60680be4044b5
Faezeh <faezehadabi9@gmail.com>
2026-08-21 11:58:31 +0330

    feat:add a loader component to the header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,6 +5,7 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 
 import Logo from "../Logo";
 import SearchHeader from "../SearchHeader";
+import NavBar from "../navBar/NavBar";
 
 const headerClasses =
   "flex justify-center p-3 items-center bg-gradient-to-r from-pink-700/90 to-pink-950/90 backdrop-blur-sm rounded-b-[45px]";
@@ -34,6 +35,9 @@ function Header() {
               </span>
             </Link>
           </div>
+        </div>
+        <div>
+          <NavBar/>
         </div>
       </header>
     </>
