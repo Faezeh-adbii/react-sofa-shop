fdd07af044c3132c09bd90ed48cfb92396eaabab
Faezeh <faezehadabi9@gmail.com>
2026-08-25 10:55:35 +0330

    fix: correct Content-Type header spelling in api config

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
     baseURL: import.meta.env.VITE_APP_GRAPHCMS,
     headers: {
-        "Content-Type": "applcation/json",
+        "Content-Type": "application/json",
         "Authorization": `Bearer ${import.meta.env.VITE_HYGRAPH_TOKEN}`,
     },
 });
