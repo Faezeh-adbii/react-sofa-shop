189de1b155750ffd5e0cadbf4c1fa5d92570a0e3
Faezeh <faezehadabi9@gmail.com>
2026-09-11 17:56:41 +0330

    feat: add responsive sofa slider with side texts

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,14 +6,22 @@ function Slider() {
     <div className=" md:flex md:justify-center md:items-center place-items-center">
       <div className="flex md:mt-0 pt-8">
         <div>
-          <span className=" text-2xl md:text-2xl lg:text-4xl font-bold text-pink-900">قیمت استثنایی</span>
+          <span className=" text-2xl md:text-2xl lg:text-4xl font-bold text-pink-900">
+            قیمت استثنایی
+          </span>
         </div>
         <div className=" text-2xl font-bold text-pink-900 md:hidden block">
           <span>، کیفیت بی نظیر</span>
         </div>
       </div>
       <div className=" mt-5 w-[80%] md:w-[60%] lg:w-[54%] md:pt-17 ">
-        <img src="/src/assets/slider/slider1.png" alt="" />
+        <img
+          src="/src/assets/slider/slider1.png"
+          alt=""
+          style={{
+            filter: "drop-shadow(0 25px 10px rgba(0,0,0,0.15))",
+          }}
+        />
       </div>
       <div className="md:block hidden  md:mt-0 pt-7  md:text-2xl lg:text-4xl md:font-bold md:text-pink-900">
         <span>کیفیت بی نظیر</span>
