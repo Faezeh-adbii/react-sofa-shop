49302493978204e014a21764954f068d22bdc12a
Faezeh <faezehadabi9@gmail.com>
2026-09-11 10:17:26 +0330

    feat: add productDetail and categoryPage routes

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -7,17 +7,21 @@ import Dashboard from "../pages/Dashboard";
 import Organization from "../pages/Organization";
 import InstallmentSales from "../pages/InstallmentSales";
 import Contact from "../pages/Contact";
+import ProductDetail from "../pages/ProductDetail";
+import CategoryPage from "../pages/CategoryPage";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="*" element={<PageNotFound/>} />
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/organization" element={<Organization />} />
       <Route path="/installmentSales" element={<InstallmentSales />} />
       <Route path="/contact" element={<Contact />} />
+      <Route path="/product/:slug" element={<ProductDetail />} />
+      <Route path="/category/:slug" element={<CategoryPage />} />
+      <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
 }
